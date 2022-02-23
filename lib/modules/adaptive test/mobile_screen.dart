b0707e40e951c75d044310a380e8cbf7b1f6ea3e
mohamedTarekMM <mohamedtarekm.22@gmail.com>
2022-02-24 00:52:08 +0200

    news App

--- lib/modules/adaptive test/mobile_screen.dart
+++ lib/modules/adaptive test/mobile_screen.dart
@@ -7,6 +7,7 @@ import '../../shared/components.dart';
 class MobileScreen extends StatelessWidget {
   //create
   //adads
+  //NewsApp
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
   @override
